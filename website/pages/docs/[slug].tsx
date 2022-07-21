855221441ce11f2ae8f21faeba7b2a003ad0c401
suneettipirneni <suneettipirneni@icloud.com>
2022-07-21 13:56:06 -0400

    fix json import

--- website/pages/docs/[slug].tsx
+++ website/pages/docs/[slug].tsx
@@ -1,10 +1,10 @@
 import { ApiModel } from "@microsoft/api-extractor-model";
 import { GetStaticProps, InferGetStaticPropsType } from "next/types";
 import path from "path";
-import * as config from "../../packageConfig.json";
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const model = new ApiModel();
+  console.log(process.cwd());
   const pkg = model.loadPackage(
     path.join(process.cwd(), "docs", "foo.api.json")
   );
@@ -23,7 +23,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 export async function getStaticPaths() {
   const model = new ApiModel();
 
-  const paths = config.packages.flatMap((name) => {
+  const paths = ["foo"].flatMap((name) => {
     const pkg = model.loadPackage(
       path.join(process.cwd(), "docs", `${name}.api.json`)
     );
